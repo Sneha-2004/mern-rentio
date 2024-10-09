bce51d06c9fbbaba7fcebf49712361cf0594de68
Sneha-Porwal <porwalsneha5@gmail.com>
2024-10-09 20:06:18 +0530

    Updated

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -21,23 +23,21 @@ export default function SignUp() {
         },
         body: JSON.stringify(formData),
       });
-
-    const data = await res.json();
-    console.log(data);
-    if (data.success === false) {
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
       setLoading(false);
-      setError(data.message);
-      return;
+      setError(null);
+      navigate('/sign-in');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
     }
-    setLoading(false);
-    setError(null);
-    navigate('/sign-in');
-  } catch (error) {
-    setLoading(false);
-    setError(error.message);
-  }
-};
-  }
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
@@ -63,12 +63,14 @@ export default function SignUp() {
           id='password'
           onChange={handleChange}
         />
-         <button
+
+        <button
           disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
           {loading ? 'Loading...' : 'Sign Up'}
         </button>
+        <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
